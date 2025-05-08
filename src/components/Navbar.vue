2c6eea7ccde65d59e572da5965e0db59966ede4f
Darkcoffe1859 <shency49@gmail.com>
2025-05-08 12:40:01 +0100

    contact

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,59 @@
-<!-- src/components/NavBar.vue -->
+<!-- src/components/Navbar.vue -->
 <template>
-  <v-app-bar app color="#1B5E20" dark>
-    <v-toolbar-title>My Portfolio</v-toolbar-title>
-    <v-spacer />
-    <v-btn text to="/">Home</v-btn>
-    <v-btn text to="/about">About</v-btn>
-    <v-btn text to="/experience">Experience</v-btn>
-    <v-btn text to="/skills">Skills</v-btn>
-    <v-btn text to="/contact">Contact</v-btn>
-  </v-app-bar>
+  <div>
+    <v-app-bar app color="#1B5E20" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none">
+        <v-icon>mdi-menu</v-icon>
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>My Portfolio</v-toolbar-title>
+
+      <v-spacer class="d-none d-md-flex" />
+
+      <!-- Nav links for medium+ screens -->
+      <div class="d-none d-md-flex">
+        <v-btn text to="/">Home</v-btn>
+        <v-btn text to="/about">About</v-btn>
+        <v-btn text to="/experience">Experience</v-btn>
+        <v-btn text to="/skills">Skills</v-btn>
+        <v-btn text to="/contact">Contact</v-btn>
+      </div>
+    </v-app-bar>
+
+    <!-- Drawer for mobile -->
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      class="d-md-none"
+    >
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/experience">
+          <v-list-item-title>Experience</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/skills">
+          <v-list-item-title>Skills</v-list-item-title>
+        </v-list-item>
+        <v-list-item link to="/contact">
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
-}
+  name: 'Navbar',
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
 </script>
